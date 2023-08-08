fa2b6d38c2fef379e51caa49fda7d4392348bc27
ashatz <ashatz.hero@gmail.com>
2023-08-08 16:57:01 -0500

    Initial app files

---
+++ app/constants.py
@@ -0,0 +1,15 @@
+APP_ENV = 'APP_ENV'
+DEFAULT_APP_ENV = 'prod'
+PROJECTS_FILE_PATH = 'PROJECTS_FILE_PATH'
+DEBUG = 'DEBUG'
+
+# Configuration file
+CONFIG_FILE_DIRECTORY = 'app/{}'
+DEFAULT_CONFIG_FILE = 'config.yml'
+CONFIG_FILE_FORMAT = 'config.{}.yml'
+APP_CONFIGURATION_FILE = 'app/schema.yml'
+
+# Configuration
+CONFIGS = 'configs' 
+ENDPOINTS = 'endpoints'
+ERRORS = 'errors'
