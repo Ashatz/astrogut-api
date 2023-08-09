252fbfadac2cf825a92d73d30ab52a75282d62c7
ashatz <ashatz.hero@gmail.com>
2023-08-09 07:35:07 -0500

    constant update

--- app/constants.py
+++ app/constants.py
@@ -1,3 +1,4 @@
+# Environment
 APP_ENV = 'APP_ENV'
 DEFAULT_APP_ENV = 'prod'
 PROJECTS_FILE_PATH = 'PROJECTS_FILE_PATH'
@@ -7,7 +8,7 @@ DEBUG = 'DEBUG'
 CONFIG_FILE_DIRECTORY = 'app/{}'
 DEFAULT_CONFIG_FILE = 'config.yml'
 CONFIG_FILE_FORMAT = 'config.{}.yml'
-APP_CONFIGURATION_FILE = 'app/schema.yml'
+APP_CONFIGURATION_FILE = 'app/app.yml'
 
 # Configuration
 CONFIGS = 'configs' 
