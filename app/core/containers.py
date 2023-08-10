ff5d78b81fb7c448be56025859421aae169e412b
ashatz <ashatz.hero@gmail.com>
2023-08-10 10:22:04 -0500

    Update container reference to reporter table

--- app/core/containers.py
+++ app/core/containers.py
@@ -3,7 +3,7 @@ from schematics import types as t, Model
 # Container configuration
 class ContainerConfiguration(Model):
     astrology_api_url = t.StringType(required=True)
-    reporter_table = t.StringType(required=True)
+    reporters_table = t.StringType(required=True)
 
 
 # Default container
@@ -14,4 +14,4 @@ class Container():
 
     def reporter_repo(self):
         from ..domains.reporter.repo.dynamodb import DynamoDBRepository
-        return DynamoDBRepository(self.config.reporter_table)
+        return DynamoDBRepository(self.config.reporters_table)
