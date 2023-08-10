1e535de9ffaa0eb6056a490bc16d0b3c000e3513
ashatz <ashatz.hero@gmail.com>
2023-08-10 09:12:01 -0500

    Add reporter table

--- app/core/containers.py
+++ app/core/containers.py
@@ -3,6 +3,7 @@ from schematics import types as t, Model
 # Container configuration
 class ContainerConfiguration(Model):
     astrology_api_url = t.StringType(required=True)
+    reporter_table = t.StringType(required=True)
 
 
 # Default container
